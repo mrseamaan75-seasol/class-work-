dec58d4a40a44eae55e0a70f6f65078fdf2ed970
mrseamaan75-seasol <mrseamaan75@gmail.com>
2025-10-07 19:46:41 +0530

    class files

---
+++ Chitti.java
@@ -0,0 +1,7 @@
+class Chitti
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hi,iam Chitti Speed 1 terahertz memory 1 zettabyte!");
+	}
+}
---
+++ Vikram.java
@@ -0,0 +1,7 @@
+class Vikram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Once upon a time there lived a gost");
+	}
+}
